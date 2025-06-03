6715f78c93c6867c9fa8777c6d1d62e32121c565
ells-s <182009067+ells-s@users.noreply.github.com>
2025-06-03 16:30:24 +0100

    added to app navigation

---
+++ app/(pages)/activity-info.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function ActivityInfo() {
+    return (
+        <View style={styles.container}>
+
+            <Text>Activity Info</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
