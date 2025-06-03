f76527320747ba679df9710e38011e2f7e95f6b8
ells-s <182009067+ells-s@users.noreply.github.com>
2025-06-03 16:30:24 +0100

    added to app navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,32 +1,32 @@
 import { Stack } from "expo-router";
-import {SafeAreaView} from "react-native-safe-area-context";
-import { StyleSheet, Text, View} from 'react-native';
-import { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 import { AuthProvider } from "@/context/UserContext";
+import { router } from 'expo-router';
 
 export default function RootLayout() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <AuthProvider>  
+    <AuthProvider>
       <SafeAreaView style={styles.container}>
         <View style={styles.box}>
           <Text>NC Holiday</Text>
+          <Pressable onPress={() => router.push('/settings')}><Text>Settings</Text></Pressable>
         </View>
         <Stack screenOptions={{ headerShown: true }} />
       </SafeAreaView>
     </AuthProvider>
-)
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    box: {
-      height: 50,
-      backgroundColor: 'beige',
-      justifyContent: 'center',
-      alignItems: 'center'
-    }
+  container: {
+    flex: 1,
+  },
+  box: {
+    height: 50,
+    backgroundColor: 'beige',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
--- firebase/signOut.js
+++ firebase/signOut.js
@@ -1,13 +1,13 @@
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebaseConfig.js';
 
-export const handleSignout = ()=>{
+export const handleSignout = () => {
 
-    return signOut (auth)
-    .then (()=>{
-        console.log(`User signed out`);
-    })
-    .catch ((err)=>{
-        console.log("Signing out error ",err.code, " ", err.message)
-    })
+    return signOut(auth)
+        .then(() => {
+            console.log(`User signed out`);
+        })
+        .catch((err) => {
+            console.log("Signing out error ", err.code, " ", err.message)
+        })
 }
