a3774d82cde483df3bddc12dea66f76d322313a0
ells-s <182009067+ells-s@users.noreply.github.com>
2025-06-04 09:43:04 +0100

    bug fix

--- app/(tabs)/_layout.js
+++
@@ -1,8 +0,0 @@
-import { Tabs } from 'expo-router';
-export default function TabLayout() {
-    return (<Tabs>
-            <Tabs.Screen name='home' options={{ title: 'Home', headerShown: false }}/>
-            <Tabs.Screen name='explore' options={{ title: 'Explore', headerShown: false }}/>
-            <Tabs.Screen name='chatbot' options={{ title: 'Chatbot', headerShown: false }}/>
-        </Tabs>);
-}
